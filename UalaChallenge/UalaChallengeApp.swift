2660629db207fc329b1573e97cb404e8f39d1c34
Martin Varela <juanmartin.varela@brubank.com>
2025-03-24 18:09:00 -0300

    Initial Commit

---
+++ UalaChallenge/UalaChallengeApp.swift
@@ -0,0 +1,17 @@
+//
+//  UalaChallengeApp.swift
+//  UalaChallenge
+//
+//  Created by Juan Martin Varela on 24/03/2025.
+//
+
+import SwiftUI
+
+@main
+struct UalaChallengeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

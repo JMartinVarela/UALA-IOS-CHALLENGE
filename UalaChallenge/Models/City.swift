a3cb5913681c520393d74c03b11826277ed784b8
Martin Varela <juanmartin.varela@brubank.com>
2025-03-24 19:50:37 -0300

    Create City model to represent cities in list

---
+++ UalaChallenge/Models/City.swift
@@ -0,0 +1,18 @@
+//
+//  City.swift
+//  UalaChallenge
+//
+//  Created by Juan Martin Varela on 24/03/2025.
+//
+
+struct City {
+    let country: String
+    let name: String
+    let id: Int
+    let coordinate: Coordinate
+}
+
+struct Coordinate {
+    let lon: Double
+    let lat: Double
+}

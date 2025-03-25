245c3180e5a0a7edcc908dfb790fc78c0f20cb40
Martin Varela <juanmartin.varela@brubank.com>
2025-03-24 21:18:18 -0300

    Added UI Tests for City list

--- UalaChallengeUITests/UalaChallengeUITests.swift
+++ UalaChallengeUITests/UalaChallengeUITests.swift
@@ -8,36 +8,26 @@
 import XCTest
 
 final class UalaChallengeUITests: XCTestCase {
-
-    override func setUpWithError() throws {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
-
-        // In UI tests it is usually best to stop immediately when a failure occurs.
+    private var app: XCUIApplication!
+    
+    override func setUp() {
         continueAfterFailure = false
-
-        // In UI tests it’s important to set the initial state - such as interface orientation - required for your tests before they run. The setUp method is a good place to do this.
-    }
-
-    override func tearDownWithError() throws {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
-    }
-
-    @MainActor
-    func testExample() throws {
-        // UI tests must launch the application that they test.
-        let app = XCUIApplication()
+        app = XCUIApplication()
         app.launch()
-
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
     }
 
-    @MainActor
-    func testLaunchPerformance() throws {
-        if #available(macOS 10.15, iOS 13.0, tvOS 13.0, watchOS 7.0, *) {
-            // This measures how long it takes to launch your application.
-            measure(metrics: [XCTApplicationLaunchMetric()]) {
-                XCUIApplication().launch()
-            }
-        }
+    override func tearDown() {
+        super.tearDown()
+        app = nil
+    }
+
+    func testListSelection() {
+        let elementsQuery = XCUIApplication().scrollViews.otherElements
+        elementsQuery.buttons["Hurzuf, UA, lat: 44,549999 long: 34,283333"].tap()
+        elementsQuery.buttons["Novinki, RU, lat: 55,683334 long: 37,666668"].tap()
+        elementsQuery.buttons["Holubynka, UA, lat: 44,599998 long: 33,900002"].tap()
+        elementsQuery.buttons["Merida, VE, lat: 8,598333 long: -71,144997"].tap()
+        elementsQuery.buttons["Lhasa, CN, lat: 29,650000 long: 91,099998"].tap()
+        elementsQuery.buttons["Istanbul, TR, lat: 41,035080 long: 28,983311"].tap()
     }
 }
